c177f2404b8924d8e53b7cf9281221492a0cfa6b
Danyon Ramsay <danyon.ramsay@yahoo.com.au>
2017-05-23 11:14:52 +1000

    Added history page UI and improved the order category menu

--- js/pages/order.js
+++ js/pages/order.js
@@ -4,6 +4,12 @@
             $("#categoryMenu").toggleClass("open", 500);
         });
 
+        $(document).on("click", function(e){
+            if (!$.contains($("#categoryMenu").get(0), e.target)) {
+                $("#categoryMenu").removeClass("open", 500);
+            }
+        });
+
         var item = new Item(1, "item", "it's an item.", 10, 1.50,
                 "https://infs3202-zwhqf.uqcloud.net/img/default_thumb.png",
                 [1, 2]);
